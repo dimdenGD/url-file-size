fdb8882dac9e7414fe37abdd1b915efe0434a3df
dimden <admin@dimden.dev>
2021-12-07 09:39:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const https = require('https');
+const http = require('http');
 
 module.exports = url => {
     return new Promise((res, rej) => {
-        let req = https.get(url);
+        let req = url.startsWith('https://') ? https.get(url) : http.get(url);
         req.once("response", r => {
             req.abort();
             let c = parseInt(r.headers['content-length']);
             if(!isNaN(c)) res(c);
             else rej("Couldn't get file size");
         });
-        req.once("error", () => rej("Connection error"));
+        req.once("error", e => rej(e));
     });
 };
