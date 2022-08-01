61d7c765247ef8394bcfbe2cd015f0ea56f335e8
dimden <admin@dimden.dev>
2022-08-01 22:14:34 +0300

    destroy instead of abort

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = url => {
     return new Promise((res, rej) => {
         let req = url.startsWith('https://') ? https.get(url) : http.get(url);
         req.once("response", r => {
-            req.abort();
+            req.destroy();
             let c = parseInt(r.headers['content-length']);
             if(!isNaN(c)) res(c);
             else rej("Couldn't get file size");
