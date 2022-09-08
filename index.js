39789bcc15b026ce31213bd5a60248e8bb9c9055
dimden <admin@dimden.dev>
2022-09-08 13:44:16 +0300

    support redirects

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const https = require('https');
 const http = require('http');
 
-module.exports = url => {
+const ufs = (url, attempts = 0) => {
     return new Promise((res, rej) => {
         let req = url.startsWith('https://') ? https.get(url) : http.get(url);
         req.once("response", r => {
             req.destroy();
+            if(r.statusCode >= 300 && r.statusCode < 400 && r.headers.location) {
+                if(attempts > 5) {
+                    return rej(new Error('Too many redirects'));
+                }
+                return res(ufs(r.headers.location, attempts + 1));
+            }
             let c = parseInt(r.headers['content-length']);
             if(!isNaN(c)) res(c);
             else rej("Couldn't get file size");
@@ -13,3 +19,5 @@ module.exports = url => {
         req.once("error", e => rej(e));
     });
 };
+
+module.exports = ufs;
